f52e82ff5544489ac4d38c7574f006763203f03d
Mihir Shete <mihirsht@gmail.com>
2016-03-24 18:17:04 -0500

    superpages: add benchmark to allocate huge page on fault

---
+++ src/ubench/pf_thp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define _2MB (2 * 1024 * 1024)
+int main(int argc, char **argv) {
+    int error, i;
+    void *p = NULL;
+    size_t alignment = _2MB;
+    size_t size = _2MB;
+    int nu_pages = 10;
+
+    error = posix_memalign(&p, alignment, size);
+    if (error != 0) {
+        perror("posix memalign");
+        exit(1);
+    }
+
+    // how much time does it take to handle this
+    // page fault is what we want to profile
+    ((char *)p)[0] = 'a';
+
+    sleep(600);
+    free(p);
+    return 0;
+}
