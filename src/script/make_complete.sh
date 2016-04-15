8f6b255a5e56825c4a9c7c79f5a19e61252a7ad8
Vikas Goel <vikasgoel.bits@gmail.com>
2016-04-15 11:50:28 -0500

    Adding restore script

--- src/script/make_complete.sh
+++ src/script/make_complete.sh
@@ -1,11 +1,43 @@
+#!/bin/bash
+
 # Navigate and make
 echo "\n\n\n\n"
 echo "=============================================="
 echo "Make Step"
 echo "=============================================="
 cd ~/vagrant/superpages/src/linux-4.1.19/
+
+:'
+if [ ! -e ".config" ] then
+	make menuconfig
+else 
+	echo "Menuconfig exists"
+fi
+'
+
 time make -j `nproc` | tee make_log
 # Check if vagrant is up
+:'
+start()
+{
+	vagrant up
+	check_machine_up
+}
+
+
+check_machine_up ()
+{
+	if [ "$status" = "running" ] then
+		echo "Machine is running\n"
+	else
+		echo "Machine not running\n"
+		start
+	fi
+}
+
+status=$(vagrant status | head -n 3 | tail -n 1 | awk '{print $2}' 2>&1)
+check_machine_up
+'
 
 # Run the make steps
 echo "\n\n\n\n"
@@ -21,5 +53,10 @@ echo "=============================================="
 vagrant ssh -c "cd /vagrant/superpages/src/linux-4.1.19/; time sudo make install -j `nproc` | tee  make_install_log"
 
 # Copy the Binaries
-vagrant ssh -c "sudo cp arch/x86_64/boot/bzImage /boot/vmlinuz-4.1.19-vik"
-vagrant ssh -c "sudo cp System.map /boot/System.map-4.1.19-vik"
+vagrant ssh -c "cd /vagrant/superpages/src/linux-4.1.19/; 
+		sudo cp arch/x86_64/boot/bzImage /boot/vmlinuz-4.1.19-vik;
+		sudo cp System.map /boot/System.map-4.1.19-vik"
+vagrant ssh -c "sudo update-grub"
+# Change the grub file for updating the correct kernel to boot from
+vagrant ssh -c ""
+
