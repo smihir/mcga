2123b4b01be5b349a81102b7b334cafe0d5e6d1f
Mihir Shete <mihirsht@gmail.com>
2016-04-20 08:09:32 -0500

    superpages: modify script to build kernel

--- scripts/make_complete.sh
+++ scripts/make_complete.sh
@@ -9,7 +9,8 @@ cd src/linux-4.1.19/
 
 time make -j `nproc` | tee linux-make.log
 # Copy the Binaries
-vagrant ssh -c "cd /vagrant/superpages/src/linux-4.1.19/; 
+vagrant ssh -c "cd /vagrant/src/linux-4.1.19/;
 		sudo cp arch/x86_64/boot/bzImage /boot/vmlinuz-4.1.19vik;
 		sudo cp System.map /boot/System.map-4.1.19vik"
 
+cd -
