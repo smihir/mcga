0b1ebf3b4a75d56a89a9fcd1fbb90c6400899c44
Mihir Shete <mihirsht@gmail.com>
2016-04-28 06:43:24 -0500

    superpages: automate the test to enable disable thp

--- src/ubench/fork_bench.sh
+++ src/ubench/fork_bench.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 gcc fork_benchmark.c -o fork_benchmark
+sudo echo always > /sys/kernel/mm/transparent_hugepage/enabled
 ./fork_benchmark 128
 ./fork_benchmark 256
 ./fork_benchmark 512
@@ -10,3 +11,15 @@ gcc fork_benchmark.c -o fork_benchmark
 #./fork_benchmark 16384
 #./fork_benchmark 32768
 #./fork_benchmark 65536
+
+sudo echo never > /sys/kernel/mm/transparent_hugepage/enabled
+./fork_benchmark 128
+./fork_benchmark 256
+./fork_benchmark 512
+./fork_benchmark 1024
+./fork_benchmark 2048
+./fork_benchmark 4096
+./fork_benchmark 8192
+
+
+sudo echo always > /sys/kernel/mm/transparent_hugepage/enabled
