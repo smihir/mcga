22d5a3dbc3c3fac5d1fc82ba911724ae3d00d707
Mihir Shete <mihirsht@gmail.com>
2016-04-28 23:22:02 -0500

    superpages: reshuffle scripts

--- scripts/restore_4.1.19.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Copy the default kernel
-vagrant ssh -c "sudo cp /boot/vmlinuz-4.1.19-vik.stable /boot/vmlinuz-4.1.19-vik; 
-		sudo cp /boot/System.map-4.1.19-vik.stable /boot/System.map-4.1.19-vik"
-vagrant ssh -c "sudo update-grub"
-# Change the grub file for updating the correct kernel to boot from
-vagrant ssh -c ""
-
