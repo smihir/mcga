6e8d24ed5d8e12833bcf65e9f2328a22879c2be0
Sejal <tia747@gmail.com>
2017-01-24 12:45:04 -0600

    Zoom out plot for latency

--- scripts/plotters/latencyplot.py
+++ scripts/plotters/latencyplot.py
@@ -1,65 +1,13 @@
-
+from __future__ import division
 __author__ = 'Sejal Chauhan'
 __author_email__ = 'sejalc@cs.wisc.edu'
 __version__ = '1.0'
-
-# run with option "3" and giving the two data files: data-thp-enabled(data1) data-thp-disabled(data2)
-#python latencyplit.py 3 data1 data2
 
 import re
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-class latency:
-
-    def __init__(self, data1):
-
-        y1 = []
-        for s in data1.split('\n'):
-            for token in s.split():
-                try:
-                    token = int(token)
-                except ValueError:
-                    token = float(token)
-            y1.append(token)
-        x1 = np.arange(len(y1))
-        fig1 = plt.figure()
-        ax1 = fig1.add_subplot(111)
-        ax1.set_title("Latency v/s Time in ms")
-        ax1.set_xlabel("Time")
-        ax1.set_ylabel("Latency")
-        ax1.plot(x1, y1, c='r')
-        plt.show()
-
-class latencyp:
-
-    def __init__(self, data1):
-
-        y1=[]
-        for s in data1.split('\n'):
-            for token in s.split():
-                try:
-                    x = re.findall("\d+", s)
-                    if len(x)>0:
-                        t = int(x[0])
-                    #if t<80 or t>90:
-                        y1.append(t)
-                except ValueError:
-                    pass
-        fig1 = plt.figure()
-        x1 = np.arange(len(y1))
-        ax1 = fig1.add_subplot(111)
-        ax1.set_title("Latency time series")
-        ax1.set_xlabel('Time in ms')
-        ax1.set_ylabel('Latency in ms')
-        ax1.plot(x1,y1)
-        frame1 = plt.gca()
-        frame1.axes.get_xaxis().set_visible(False)
-        ax1.set_ylim([-5,600])
-        #fig1.subplots_adjust(bottom=1.2)
-        #pl.setp(labels, rotation=90)
-        plt.show()
 
 class latencyboth:
 
@@ -69,15 +17,23 @@ class latencyboth:
         y2=[]
 
         for s1 in data1.split('\n'):
-            for token in s1.split():
-                try:
-                    token = int(token)
-                except ValueError:
-                    token = float(token)
-            y1.append(token)
+            try:
+                x1 = re.findall("\d+", s1)
+                if len(x1) > 0:
+                    t = int(x1[0])
+                    y1.append(t)
+            except ValueError:
+                pass
+            #for token in s1.split():
+                #try:
+                    #x1 = re.findall("\d+", s1)
+                    #token = int(x1[0])
+                #except ValueError:
+                 #   token = float(token)
+                #y1.append(token)
 
         for s2 in data2.split('\n'):
-            for token in s2.split():
+            #for token in s2.split():
                 try:
                     x2 = re.findall("\d+", s2)
                     if len(x2) > 0:
@@ -86,26 +42,94 @@ class latencyboth:
                 except ValueError:
                     pass
         fig1 = plt.figure()
-        x2 = np.arrange(len(y2))
+        x1 = np.arange(len(y1))
+        x2 = np.arange(len(y2))
         ax1 = fig1.add_subplot(111)
         ax1.set_title("Latency time series")
-        ax1.set_xlabel('Time in ms')
-        ax1.set_ylabel('Latency in ms')
-        ax1.plot(x2, y1,color='r', marker='o')
-        ax1.plot(x2, y2, color='g', marker='^')
+        ax1.set_xlabel('Time (ms)')
+        ax1.set_ylabel('Latency (ms)')
+        rects1 = ax1.plot(x1, y1,color='r')#, marker='o')
+        rects2 = ax1.plot(x2, y2, color='g')#, marker='^')
         frame1 = plt.gca()
-        frame1.axes.get_xaxis().set_visible(False)
+        #frame1.axes.get_xaxis().set_visible(False)
+        ax1.set_ylim([0, 200])
+        ax1.set_xlim([0, min(len(x1),len(x2))])
+        ax1.legend((rects1[0], rects2[0]), ('THP Disabled', 'THP Enabled'), shadow=False, loc='upper left')
+
+        ax2 = plt.axes([.65, .6, .2, .2], axisbg='w')
+        ax2.plot(x1, y1, color='r')
+        ax2.plot(x2, y2, color='g')
+        ax2.set_xlim([0, min(len(x1), len(x2))])
+        #y2labels = ['0', '500', '1000', '1400']
+        plt.setp(ax2, xticks=[], yticks=[])
+        #ax2.yticks(y2, y2labels)
+        #ax2.set_yticklabels(y2labels)
+        plt.show()
+
+class rssanon:
+    def __init__(self, data1):
+
+        y1=[]
+        y2=[]
+        for s in data1.split('\n'):
+            for token in s.split():
+                try:
+                    x = re.findall("\d+", s)
+                    if len(x) > 0:
+                        t1 = int(x[0])
+                        t2 = int(x[1])
+                        y1.append(t1)
+                        y2.append(t2)
+                except ValueError:
+                    pass
+
+            fig1 = plt.figure()
+            x2 = np.arange(len(y2))
+            ax1 = fig1.add_subplot(111)
+            ax1.set_title("Rss and AnonHugePages")
+            ax1.set_xlabel('Time in s')
+            ax1.set_ylabel('Rss and AnonHugePages')
+            ax1.plot(x2, y1, color='r')#, marker='o')
+            ax1.plot(x2, y2, color='g')#, marker='^')
+            frame1 = plt.gca()
+            frame1.axes.get_xaxis().set_visible(False)
+
+            plt.show()
+
+class forkplot:
+
+    def __init__(self):
+        #x = [129, 258, 515, 1031, 2061, 4121, 8241]
+
+        y1 = [231, 400, 725, 1282, 2512]
+        y2= [1263, 2427, 4881, 8589, 16973]
+        x = np.arange(len(y1))
+        fig1 = plt.figure()
+        ax1 = fig1.add_subplot(111)
+        ax1.set_title("Time taken to Fork v/s RSS")
+        ax1.set_ylabel('Time in ms')
+        ax1.set_xlabel('RSS')
+        y1 = [y/1000 for y in y1]
+        y2 = [y/1000 for y in y2]
+        rects1 = ax1.plot(x, y1, color='r', marker='o')
+        rects2 = ax1.plot(x, y2, color='g', marker='^')
+        xlabel = ['128MB', '256MB', '512MB', '1GB', '2GB']
+        plt.xticks(x, xlabel)
+        ax1.set_xticklabels(xlabel, rotation='horizontal')
+        ax1.legend((rects1[0], rects2[0]), ('THP Disabled', 'THP Enabled'), shadow=False, loc='upper left')
         plt.show()
 
 if __name__ == '__main__':
-    with open(sys.argv[1], 'rb') as f1:
-        data1 = f1.read()
 
-    with open(sys.argv[2], 'rb') as f2:
-        data2 = f2.read()
-        if sys.argv[2] == '1':
-            latency(data1)
-        if sys.argv[2] == '2':
-            latencyp(data1)
-        if sys.argv[2] == '3':
+        if sys.argv[1] == '1':
+            with open(sys.argv[2], 'rb') as f1:
+                data1 = f1.read()
+            with open(sys.argv[3], 'rb') as f2:
+                data2 = f2.read()
             latencyboth(data1, data2)
+        if sys.argv[1] == '2':
+            with open(sys.argv[2], 'rb') as f1:
+                data1 = f1.read()
+            rssanon(data1)
+        if sys.argv[1] == '3':
+            forkplot()
