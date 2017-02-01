b425590eee0fb80e68da660a77a45c9fabf1254b
f2008700 <vikasgoel.bits@gmail.com>
2017-01-31 20:17:48 -0600

    mcga: sweeping result of smap to the file

--- scripts/plotters/getRssAnonHugePages.sh
+++ scripts/plotters/getRssAnonHugePages.sh
@@ -12,12 +12,13 @@ do
     if [ ! -d /proc/$pid ];
     then
         grep thp_promote thpvmstat.out | awk '{print $2}' > thppromote.out
-        grep thp_collapse_alloc thpvmstat.out | awk '{print $2}' > thp_collapse_alloc.out
+        grep "thp_collapse_alloc " thpvmstat.out | awk '{print $2}' > thp_collapse_alloc.out
         break;
     fi
     a=$(cat /proc/$pid/smaps | grep Rss: | awk '{SUM += $2} END { print SUM}') 
     b=$(cat /proc/$pid/smaps | grep AnonHugePages: | awk '{SUM += $2} END { print SUM}')
     ps -C "khugepaged" -L -o %cpu=,psr= >> khugepagedcpu.out
     cat /proc/vmstat | grep thp >> thpvmstat.out
+    echo $a $b | tee -a $result    
     sleep 1
 done
