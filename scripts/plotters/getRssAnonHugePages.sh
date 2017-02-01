4d1717b11a6add65498fe548104bdfcca7be467f
Mihir Shete <mihirsht@gmail.com>
2017-01-31 19:45:41 -0600

    mcga: modify log collection scripts

--- scripts/plotters/getRssAnonHugePages.sh
+++ scripts/plotters/getRssAnonHugePages.sh
@@ -3,12 +3,21 @@
 # information and stores it in <#Rss> <#Anon> format.
 
 result=smap.out
-rm $result
-pid=`pgrep redis-ser`
+rm -rf $result
+rm -rf khugepagedcpu.out
+rm -rf thpvmstat.out
+pid=`pgrep redis-server`
 while true
-do 
+do
+    if [ ! -d /proc/$pid ];
+    then
+        grep thp_promote thpvmstat.out | awk '{print $2}' > thppromote.out
+        grep thp_collapse_alloc thpvmstat.out | awk '{print $2}' > thp_collapse_alloc.out
+        break;
+    fi
     a=$(cat /proc/$pid/smaps | grep Rss: | awk '{SUM += $2} END { print SUM}') 
     b=$(cat /proc/$pid/smaps | grep AnonHugePages: | awk '{SUM += $2} END { print SUM}')
-    echo $a $b | tee -a $result
+    ps -C "khugepaged" -L -o %cpu=,psr= >> khugepagedcpu.out
+    cat /proc/vmstat | grep thp >> thpvmstat.out
     sleep 1
 done
