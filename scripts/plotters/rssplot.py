214ea9892e4e000daf4c85f9b0aee54fb56f904f
Sejal <tia747@gmail.com>
2017-02-01 19:58:01 -0600

    Updating the rss area curve script

--- scripts/plotters/rssplot.py
+++ scripts/plotters/rssplot.py
@@ -6,63 +6,43 @@ import re
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 class rssvstime:
 
     def __init__(self, data1):
 
-        #thp enabled
         y1=[]
-        x1 =[]
-        #thp disabled
         y2=[]
-        x2=[]
-        value= "Transparent Huge Pages Disabled"
-        enabled=1
-        data = data1.split('\n')
-        data2=[]
-        for s in data:
-            print s
-            if (s == value) and not bool(re.search(r'\d', s)):
-                enabled=0
+        for s in data1.split('\n'):
+            try:
+                x = re.findall("\d+", s)
 
-            if(enabled==1):
-                try:
-                    x = re.findall("\d+", s)
-                    if len(x)>0:
-                        t = int(x[0])
-                        r = int(x[2])
-                        y1.append(r)
-                        x1.append(t)
-                except ValueError:
-                    pass
-            if(enabled==0):
-                try:
-                    x = re.findall("\d+", s)
-                    if len(x)>0:
-                        t = int(x[0])
-                        r = int(x[2])
-                        y2.append(r)
-                        x2.append(t)
-                except ValueError:
-                    pass
+                if len(x) > 0:
+                    t1 = int(x[0])
+                    t2 = int(x[1])
+                    y1.append(t1)
+                    y2.append(t2)
+            except ValueError:
+                pass
 
         fig1 = plt.figure()
+        x1 = np.arange(len(y1))
         ax1 = fig1.add_subplot(111)
-        ax1.set_title("Resident Set Size v/s Time")
-        ax1.set_xlabel('RSS in MB')
-        ax1.set_ylabel('Time in ms')
-        y1 = [x / 1000 for x in y1]
-        y2 = [x / 1000 for x in y2]
-        ax1.plot(x1,y1, c='r', label='THP enabled')
-        ax1.plot(x2,y2, c='b', label='THP disabled')
-        xlabels = ['128', '256', '512', '1024', '2048', '4096', '8192', '16384', '65536', '32768']
-        ax1.set_xticklabels(xlabels, rotation='vertical')
-        plt.xticks(x1, xlabels)
-        ax1.plot(x1,y1, 'ro', x2, y2, 'gs')
-        ax1.set_xscale('log', basex=2)
-        legend = ax1.legend(loc='upper center', shadow=True)
-        frame = legend.get_frame()
+        ax1.set_xlabel('Time (min)')
+        ax1.set_ylabel('RSS (GB)')
+        x1 = [x / 60 for x in x1]
+        y1 = [y / 1048576 for y in y1]
+        y2 = [y / 1048576 for y in y2]
+        ax1.plot(x1, y1, color='yellow', label='Base Pages')
+        ax1.plot(x1, y2, color='orange', label='Large Pages')
+        ax1.fill_between(x1, 0, y1, facecolor = 'yellow', interpolate=True)
+        ax1.fill_between(x1, 0, y2, facecolor = 'orange', interpolate=True)
+        ax1.set_xlim([0, x1[-1]])
+        ax1.set_ylim([0, 1.2* max(max(y1),max(y2))])
+        yellow_patch = mpatches.Patch(color='yellow', label='Base Pages')
+        orange_patch = mpatches.Patch(color='orange', label='Large Pages')
+        plt.legend([yellow_patch, orange_patch], ["Memory backed by Base Pages", "Memory backed by Large Pages"], loc='upper left')
         plt.show()
 
 if __name__ == '__main__':
